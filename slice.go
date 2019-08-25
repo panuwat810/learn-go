f65a6a5c1f3aacec786e09e588958297f66b0dcc
panuwat ketwong <panuwat.ket@lives.rmu.ac.th>
2019-08-25 11:54:57 +0700

    list slice values

--- slice.go
+++ slice.go
@@ -1,11 +1,8 @@
 package main
 
-import "fmt"
-
 func main() {
 	p := []int{2, 3, 5, 7, 11, 13}
-	fmt.Println(p)
-	fmt.Println(p[0:3])
-	fmt.Println(p[3:5])
-	fmt.Println(p[4:])
+	for _, value := range p {
+		println(value)
+	}
 }
